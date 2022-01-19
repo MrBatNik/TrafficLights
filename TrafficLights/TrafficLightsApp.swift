63a0354b1fefa7aed5aa741afc34e006001daf9c
Nikita <nicandreo@gmail.com>
2022-01-19 21:40:40 +0300

    Initial Commit

---
+++ TrafficLights/TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp.swift
+//  TrafficLights
+//
+//  Created by Nikita Batrakov on 19.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
