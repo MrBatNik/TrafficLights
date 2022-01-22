9170b16a7e183bfb5b9c75a70e3a98fa1870af7b
Nikita <nicandreo@gmail.com>
2022-01-22 13:14:39 +0300

    remove padding from circle/ add dictionary for opacity

--- TrafficLights/CircleView.swift
+++ TrafficLights/CircleView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct CircleView: View {
     let color: Color
-    var opacity: Double
+    let opacity: Double
     
     var body: some View {
         Circle()
@@ -17,7 +17,6 @@ struct CircleView: View {
             .opacity(opacity)
             .frame(width: 100, height: 100)
             .overlay(Circle().stroke(Color.white, lineWidth: 5))
-            .padding()
     }
 }
 
--- TrafficLights/ContentView.swift
+++ TrafficLights/ContentView.swift
@@ -9,37 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var buttonTitle = "START"
-    @State private var redLight = CircleView(color: .red, opacity: 0.3)
-    @State private var yellowLight = CircleView(color: .yellow, opacity: 0.3)
-    @State private var greenLight = CircleView(color: .green, opacity: 0.3)
+    @State private var viewSetups: [String: Double] = [
+        "red": 0.3,
+        "yellow": 0.3,
+        "green": 0.3
+    ]
     
     var body: some View {
         ZStack {
             Color(.black)
                 .ignoresSafeArea()
-            VStack {
-                redLight
-                yellowLight
-                greenLight
+            VStack(spacing: 20) {
+                CircleView(color: .red, opacity: viewSetups["red"] ?? 0)
+                CircleView(color: .yellow, opacity: viewSetups["yellow"] ?? 0)
+                CircleView(color: .green, opacity: viewSetups["green"] ?? 0)
                 Spacer()
-                Button {
-                    buttonTitle = "NEXT"
-                    if redLight.opacity == 1 {
-                        redLight.opacity = 0.3
-                        yellowLight.opacity = 1
-                    } else if yellowLight.opacity == 1 {
-                        yellowLight.opacity = 0.3
-                        greenLight.opacity = 1
-                    } else {
-                        greenLight.opacity = 0.3
-                        redLight.opacity = 1
-                    }
-                } label: {
+                Button(action: tapButton) {
                     Text(buttonTitle)
                 }
                     .buttonStyle(RoundedButtonStyle())
             }
             .padding()
+        }
+    }
+    
+    private func tapButton() {
+        buttonTitle = "NEXT"
+        if viewSetups["red"] == 1 {
+            viewSetups["red"] = 0.3
+            viewSetups["yellow"] = 1
+        } else if viewSetups["yellow"] == 1 {
+            viewSetups["yellow"] = 0.3
+            viewSetups["green"] = 1
+        } else {
+            viewSetups["green"] = 0.3
+            viewSetups["red"] = 1
         }
     }
 }
